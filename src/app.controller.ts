fdf3e80dea1a86c91a90b9ad94b66fb055c14b18
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2025-05-04 21:35:35 +0900

    first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  @Get()
+  getHello() {
+    return 'Hello';
+  }
+  // constructor(private readonly appService: AppService) {}
+
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+}
