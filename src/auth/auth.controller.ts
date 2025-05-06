330ed9b2854c155a41f35cf7337debec865464f0
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2025-05-06 19:04:07 +0900

    May-6

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,15 @@
-import { Controller, Post } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-  @Post()
+  @Post('login')
+  create(@Body(ValidationPipe) createUser: CreateUserDto) {
+    return this.authService.login(createUser);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,26 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/require-await */
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly usersService: UsersService,
+  ) {}
+  async validateUser({ username, password }: CreateUserDto) {
+    const user = this.usersService.findOne(username);
+    return true;
+  }
+  async login(user: CreateUserDto) {
+    if (await this.validateUser(user)) {
+      const payload = { username: user.username };
+      return {
+        access_token: this.jwtService.sign(payload),
+      };
+    }
+  }
+}
