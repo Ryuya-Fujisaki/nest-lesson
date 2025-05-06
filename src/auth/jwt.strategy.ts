33e0945ae929f8c06dd3253d4de5443c7a669a7f
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2025-05-06 20:10:50 +0900

    May-6

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,21 @@
+/* eslint-disable @typescript-eslint/require-await */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'secret',
+    });
+  }
+  async validate(payload: JwtPayload) {
+    return payload;
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unused-vars */
@@ -8,10 +9,13 @@ import {
   Get,
   Param,
   Post,
+  UseGuards,
   ValidationPipe,
+  Request,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('users')
 export class UsersController {
@@ -21,7 +25,9 @@ export class UsersController {
     return this.usersService.findAll();
   }
   @Get(':username')
-  findOne(@Param('username') username: string) {
+  @UseGuards(AuthGuard('jwt'))
+  findOne(@Param('username') username: string, @Request() req: any) {
+    return req.user;
     return this.usersService.findOne(username);
   }
   @Post()
